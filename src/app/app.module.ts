d7435ecf5e474ee9c318ab50d9d0947ca5a602ea
Shubham Saxena <shubh3695@gmail.com>
2017-10-06 23:55:51 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatMenuModule, MatButtonModule, MatIconModule, MatCardModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { MenudemoComponent } from './menudemo/menudemo.component';
+import { SidenavdemoComponent } from './sidenavdemo/sidenavdemo.component';
+import { ToolbardemoComponent } from './toolbardemo/toolbardemo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenudemoComponent,
+    SidenavdemoComponent,
+    ToolbardemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatCardModule, MatButtonModule, MatIconModule, MatMenuModule, BrowserAnimationsModule,
+    MatSidenavModule, MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
